dc8d18d8fb14aa5d43e89d4822b395404a6f3ad0
=juslev <reyzae99@gmail.com>
2021-03-16 21:56:10 +0700

    import data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('user')->insert(([
+        DB::table('users')->insert(([
             'name'=>'Administrator',
             'email'=>'admin@admin.com',
             'password'=>Hash::make('password')
