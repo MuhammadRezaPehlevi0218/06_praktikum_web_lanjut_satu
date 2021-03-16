b2be99c686f8a9cfaff145519e0f7bef3402968f
=juslev <reyzae99@gmail.com>
2021-03-16 22:41:22 +0700

    membenahi langkah sebelumnya yang salah

--- database/migrations/2021_03_16_153729_tambah_kolom_di_tabel_user2.php
+++ database/migrations/2021_03_16_153729_tambah_kolom_di_tabel_user2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class TambahKolomDiTabelUser extends Migration
+class TambahKolomDiTabelUser2 extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class TambahKolomDiTabelUser extends Migration
      */
     public function up()
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('username',20)->after('id')->Nullable()->Unique();
         });
     }
